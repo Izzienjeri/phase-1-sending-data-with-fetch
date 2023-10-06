0b471f2cb3174181e474bd79e4ae902d987b6b48
izzie <izzienjeri12@gmail.com>
2023-10-07 01:49:09 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+function submitData (userName, userEmail) {
+    return fetch('http://localhost:3000/users', 
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body:JSON.stringify({
+            name: userName,
+            email: userEmail,
+        }),
+    }
+    ) 
+    .then(function (response) {
+        return response.json();
+      })
+    .then(function(data){
+        console.log(data)
+    })
+    .catch(function (error) {
+        console.log('Error!', error);
+    
+
+    const errorContainer = document.createElement('div');
+    errorContainer.textContent = 'Error: ' + error.message;
+    document.body.appendChild(errorContainer);
+      });
+
+};
+
+submitData('izzie', 'izzienjeri12@gmail.com')
+
+
+
